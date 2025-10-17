2c7659dfab5ce3ec75e0a0ed2d6e56564f99f92c
John Lafferty <3231101+astralvoid2@users.noreply.github.com>
2025-10-17 14:11:07 -0400

    feature: add sorted printout of characters in book

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import number_of_words
+from stats import number_of_words, character_count_per_letter, get_sorted_character_counts
 
 def get_book_text(filepath):
     with open(filepath, 'r') as file:
@@ -6,10 +6,21 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    bookText = get_book_text('books/frankenstein.txt')    
-    num_words = number_of_words(bookText)
+    bookpath = 'books/frankenstein.txt'
+    bookText = get_book_text(bookpath)    
+    num_words = number_of_words(bookText)    
+    char_counts = character_count_per_letter(bookText)    
+    sorted_char_counts = get_sorted_character_counts(char_counts)    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {bookpath}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for letter in sorted_char_counts:
+        print(f"{letter['letter']}: {letter['num']}")    
+    print("============= END ===============")
     
     
-
+    
+    
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
 def number_of_words(text):
     words = text.split()
     return len(words)
+
+
+def character_count_per_letter(text):
+    letter_counts = {}
+    for letter in text:
+        check_letter = letter.lower()
+        if check_letter in letter_counts:
+            letter_counts[check_letter] += 1
+        else:
+            letter_counts[check_letter] = 1
+            
+    return letter_counts
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_character_counts(count_dictionary):
+    sorted_char_counts = []
+    for key, item in count_dictionary.items():
+        if key.isalpha():
+            sorted_char_counts.append({"letter": key, "num": item})
+
+    sorted_counts = sorted(sorted_char_counts, reverse=True, key=sort_on)
+    return sorted_counts
