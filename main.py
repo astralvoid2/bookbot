6e7c90f9026aa1a6551551a12de2403678e033ea
John Lafferty <3231101+astralvoid2@users.noreply.github.com>
2025-10-17 13:20:48 -0400

    feature: word counter of book

---
+++ main.py
@@ -0,0 +1,18 @@
+def get_book_text(filepath):
+    with open(filepath, 'r') as file:
+        file_contents = file.read()
+    return file_contents
+
+def number_of_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    bookText = get_book_text('books/frankenstein.txt')
+    # print(bookText)
+    num_words = number_of_words(bookText)
+    print(f"Found {num_words} total words")
+    
+    
+
+main()
