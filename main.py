120f9b535b16a92e75d9c9887c858314d8b10efe
John Lafferty <3231101+astralvoid2@users.noreply.github.com>
2025-10-17 13:22:59 -0400

    refactor: moved word count function to separate file

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import number_of_words
+
 def get_book_text(filepath):
     with open(filepath, 'r') as file:
         file_contents = file.read()
     return file_contents
-
-def number_of_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     bookText = get_book_text('books/frankenstein.txt')
